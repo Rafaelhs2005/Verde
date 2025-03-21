61ff0e64d7e8d60153985bdcabe0efaac4d286ab
Rafael Henrique Silva <rhenriques.2005@gmail.com>
2025-03-21 19:16:35 -0300

    Update alternador.c

--- Atividades/alternador.c
+++ Atividades/alternador.c
@@ -6,23 +6,19 @@ void combinador(char *str1, char *str2, char *resultado) {
     int len2 = strlen(str2);
     int i = 0, j = 0, k = 0;
 
-    // Alterna as letras das duas strings
     while (i < len1 && j < len2) {
         resultado[k++] = str1[i++];
         resultado[k++] = str2[j++];
     }
 
-    // Adiciona as letras restantes da primeira string, se houver
     while (i < len1) {
         resultado[k++] = str1[i++];
     }
 
-    // Adiciona as letras restantes da segunda string, se houver
     while (j < len2) {
         resultado[k++] = str2[j++];
     }
 
-    // Finaliza a string resultante
     resultado[k] = '\0';
 }
 
